b4aaafb6a0efcd50380916a80465f25577079cee
unknown <magnus_pascal.bergmann@mailbox.tu-dresden.de>
2024-06-21 21:08:20 +0200

    Added Junit5 to the project and added trueTest.java with a test that should never fail

---
+++ src/test/java/org/discord/bot/eckort/trueTest.java
@@ -0,0 +1,13 @@
+package org.discord.bot.eckort;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+
+public class trueTest {
+
+    @Test
+    void Idempotence(){
+        assertEquals(true, true);
+    }
+}
